c906fde193b811c071698984869ea2bf0e2cb804
Hongwei Xi <gmhwxi@gmail.com>
2019-07-06 14:57:41 -0400

    Updating: very very minorly

--- contrib/sazl/appveyor/install-ats2.sh
+++ contrib/sazl/appveyor/install-ats2.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
+
+######
+
 set -e
 
-ATS2VERSION=$1
-ATS2PKG=ATS2-Postiats-$ATS2VERSION
+######
+
+ATS2VER=$1
+ATS2PKG=\
+ATS2-Postiats-$ATS2VER
 ATS2PKGTGZ=$ATS2PKG.tgz
-ATS2HOME=$2 #${PATSHOME:-$2}
-ATS2URL="https://sourceforge.net/projects/ats2-lang/files/ats2-lang/ats2-postiats-$ATS2VERSION/$ATS2PKGTGZ/download"
+
+######
+
+ATSLANGURL_srcfg=\
+https://sourceforge.net/projects/ats2-lang
+
+######
+
+ATS2URL=\
+"${ATSLANGURL_srcfg}/files/ats2-lang/ats2-postiats-$ATS2VER/$ATS2PKGTGZ"
+
+######
+
+ATS2HOME=$2 # =${PATSHOME}
+
+######
 
 mkdir -p $ATS2HOME
-cd $ATS2HOME
-wget -qO- $ATS2URL | tar xz -C $ATS2HOME --strip 1
 
-./configure
-make all
+######
+
+( \
+cd $ATS2HOME && \
+wget -qO- $ATS2URL | \
+tar xz -C $ATS2HOME --strip 1 \
+)
+
+######
+
+(cd $ATS2HOME && ./configure && make all)
+
+######
 
 ###### end of [install-ats2.sh] ######
