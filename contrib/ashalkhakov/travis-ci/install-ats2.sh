00f4921f4b13d574b9546db6c713e78c0bb61ae7
Hongwei Xi <gmhwxi@gmail.com>
2019-07-06 14:57:41 -0400

    Updating: very very minorly

--- contrib/ashalkhakov/travis-ci/install-ats2.sh
+++ contrib/ashalkhakov/travis-ci/install-ats2.sh
@@ -10,32 +10,33 @@ ATSPACKTGZ=$ATSPACK.tgz
 ######################################
 
 MV=mv
-WGETQ="wget -q"
+WGET="wget"
 TARZXF="tar zxf"
 
 ATSLANGURL_srcfg=\
 https://sourceforge.net/projects/ats2-lang
 
-cd $HOME
-$WGETQ ${ATSLANGURL_srcfg}/files/ats2-lang/ats2-postiats-$ATSVER/$ATSPACKTGZ
-
 ######################################
 
-$TARZXF ${ATSPACKTGZ}
+$ATSPACKTGZURL_download =\
+${ATSLANGURL_srcfg}/files\
+/ats2-lang/ats2-postiats-$ATSVER/$ATSPACKTGZ
 
 ######################################
 #
 # HX-2019-06:
 # ATSPACKgmp is the default
 #
-ATSPACKint=\
-ATS2-Postiats-int-$ATSVER
-ATSPACKgmp=\
-ATS2-Postiats-gmp-$ATSVER
+# ATSPACKint=\
+# ATS2-Postiats-int-$ATSVER
+# ATSPACKgmp=\
+# ATS2-Postiats-gmp-$ATSVER
+#
 ######################################
-#
-(${MV} -f ${ATSPACKgmp} ${HOME}/ATS2)
-#
+
+(cd $HOME &&
+$WGET -qO- $ATSPACKTGZURL_download | $TARZXF -C ATS2 --strip 1)
+
 ######################################
 
 ###### end of [install-ats2.sh] ######
