fb7507ea21f22da293a8e769a93c9bc07a4bd04d
Hongwei Xi <gmhwxi@gmail.com>
2020-05-24 15:00:43 -0400

    Updating: very very minorly

---
+++ xatsopt/runtime/CC/xatsopt_basics.h
@@ -0,0 +1,58 @@
+/* ******************************************************************* */
+/*                                                                     */
+/*                         Applied Type System                         */
+/*                                                                     */
+/* ******************************************************************* */
+
+/*
+** ATS/Postiats - Unleashing the Potential of Types!
+** Copyright (C) 2011-20?? Hongwei Xi, ATS Trustful Software, Inc.
+** All rights reserved
+**
+** ATS is free software;  you can  redistribute it and/or modify it under
+** the terms of  the GNU GENERAL PUBLIC LICENSE (GPL) as published by the
+** Free Software Foundation; either version 3, or (at  your  option)  any
+** later version.
+** 
+** ATS is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without  even  the  implied  warranty  of MERCHANTABILITY or
+** FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License
+** for more details.
+** 
+** You  should  have  received  a  copy of the GNU General Public License
+** along  with  ATS;  see the  file COPYING.  If not, please write to the
+** Free Software Foundation,  51 Franklin Street, Fifth Floor, Boston, MA
+** 02110-1301, USA.
+*/
+
+/* ****** ****** */
+
+/*
+(* Author: Hongwei Xi *)
+(* Start time: May 24, 2020 *)
+(* Authoremail: gmhwxiATgmailDOTcom *)
+*/
+
+/* ****** ****** */
+
+#ifndef XATSOPT_BASICS_H
+#define XATSOPT_BASICS_H
+
+/* ****** ****** */
+//
+// HX: boolean values
+//
+#define xatsopt_bool_true 1
+#define xatsopt_bool_false 0
+//
+/* ****** ****** */
+
+#define xatsopt_ptr0_null ((void*)0)
+
+/* ****** ****** */
+
+#endif /* XATSOPT_BASICS_H */
+
+/* ****** ****** */
+
+/* end of [xatsopt_basics.h] */
