425502c41af3d2de5d2d65e3944b8daa33863f22
Hongwei Xi <gmhwxi@gmail.com>
2021-01-09 01:37:19 -0500

    Updating: very very minorly

--- contrib/bbarker/NIX/tarball.nix
+++ contrib/bbarker/NIX/tarball.nix
@@ -17,38 +17,27 @@
 , autoconf
 , automake
 , version
-}:
+} :
 
 stdenv.mkDerivation rec {
-  name = "ATS3-Xanadu-${version}.tgz";
+  name =
+  "ATS3-Xanadu-${version}.tgz";
 
-  buildInputs = [ autoconf automake boehmgc gmp ];
+  buildInputs =
+  [ autoconf automake boehmgc gmp ];
 
-  src = builtins.filterSource (path: type:
-    (toString path) != (toString ../.git)
-  ) ../.;
+  src =
+  builtins.filterSource
+  ( path: type:
+    (toString path) != (toString ../.git) ) ../.;
 
-  PATSHOME = "${ats2}/lib/ats2-postiats-${ats2.version}";
-  PATSHOMERELOC = "${PATSHOME}";
-  
-  # configurePhase = ''
-  #  patchShebangs doc/DISTRIB/ATS-Postiats/autogen.sh
-  #  export XATSHOME=$PWD
-  #  make -f codegen/Makefile_atslib
-  # '';
- 
-  # buildPhase = ''
-  #   make -f srcgen/Makefile_stat
-  # '';
-
-  # installPhase = ''
-  #   mv doc/DISTRIB/${name} $out
-  # '';
+  PATSHOME =
+  "${ats2}/lib/ats2-postiats-${ats2.version}";
 
   shellHook = ''
     export XATSHOME=$PWD
     source ./share/NIX/pathenv.sh
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:"${boehmgc}/lib"
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"${boehmgc}/lib"
   '';
 }
 
