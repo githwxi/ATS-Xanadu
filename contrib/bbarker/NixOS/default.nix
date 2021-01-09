6b545d9aa65ec21e07b806edc064bb175be57fac
Hongwei Xi <gmhwxi@gmail.com>
2021-01-08 23:25:25 -0500

    Updating: minorly fixing nix scripts

--- contrib/bbarker/NixOS/default.nix
+++ contrib/bbarker/NixOS/default.nix
@@ -10,7 +10,7 @@ let
   pkgs    = import <nixpkgs> {};
 
   version = builtins.head (builtins.match "([^\n]*)\n?" (
-    builtins.readFile ../VERSION
+    builtins.readFile ./VERSION
   ));
 in rec {
   tarball = pkgs.callPackage ./tarball.nix { inherit version; };
