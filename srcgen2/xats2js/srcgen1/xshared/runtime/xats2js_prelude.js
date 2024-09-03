95b9b778255527a6124e0ecf882c5ac6a2b5f905
Hongwei <gmhwxi@gmail.com>
2024-09-03 18:45:24 -0400

    Updating: very very minorly

--- srcgen2/xats2js/srcgen1/xshared/runtime/xats2js_prelude.js
+++ srcgen2/xats2js/srcgen1/xshared/runtime/xats2js_prelude.js
@@ -1,4 +1,4 @@
-// Tue 03 Sep 2024 06:13:50 PM EDT
+// Tue 03 Sep 2024 06:34:55 PM EDT
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 /*
@@ -374,5 +374,211 @@ XATS2JS_strn_fmake_fwork
 ////////////////////////////////////////////////////////////////////////.
 ////////////////////////////////////////////////////////////////////////.
 // end of [ATS3/XANADU_prelude_DATS_CATS_JS_strn000.cats]
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+//                                                                    //.
+//                         Applied Type System                        //.
+//                                                                    //.
+////////////////////////////////////////////////////////////////////////.
+
+/*
+** ATS/Xanadu - Unleashing the Potential of Types!
+** Copyright (C) 2024 Hongwei Xi, ATS Trustful Software, Inc.
+** All rights reserved
+**
+** ATS is free software;  you can  redistribute it and/or modify it under
+** the terms of  the GNU GENERAL PUBLIC LICENSE (GPL) as published by the
+** Free Software Foundation; either version 3, or (at  your  option)  any
+** later version.
+** 
+** ATS is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without  even  the  implied  warranty  of MERCHANTABILITY or
+** FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License
+** for more details.
+** 
+** You  should  have  received  a  copy of the GNU General Public License
+** along  with  ATS;  see the  file COPYING.  If not, please write to the
+** Free Software Foundation,  51 Franklin Street, Fifth Floor, Boston, MA
+** 02110-1301, USA.
+*/
+
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+/*
+Author: Hongwei Xi
+Mon 12 Aug 2024 09:36:59 AM EDT
+Authoremail: gmhwxiATgmailDOTcom
+*/
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+//
+function
+XATS2JS_a0rf_lget
+  ( A )
+{
+  return A[0]
+}
+function
+XATS2JS_a0rf_lset
+  (A, x)
+{
+  A[0] = x; return
+}
+//
+function
+XATS2JS_a0rf_make_1val
+  ( x0 )
+{
+  return [x0] // HX: [a0rf] is singleton
+}
+//
+////////////////////////////////////////////////////////////////////////.
+//
+function
+XATS2JS_a1rf_lget$at
+  (A, i)
+{
+  return A[i]
+}
+function
+XATS2JS_a1rf_lset$at
+  (A, i, x)
+{
+  A[i] = x; return
+}
+//
+function
+XATS2JS_a1rf_make_ncpy
+  (n, x)
+{
+  var i = 0
+  var A = new Array(n);
+  while (i < n) {
+    A[i] = (x); i += 1
+  }
+  return A // HX: A = [x, x, ..., x]
+}
+//
+function
+XATS2JS_a1rf_make_nfun
+  (n, f)
+{
+  var i = 0
+  var A = new Array(n);
+  while (i < n) {
+    A[i] = f(i); i += 1
+  }
+  return A // HX: A = [f(0),f(1),...,f(n)]
+}
+//
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+// end of [ATS3/XANADU_prelude_DATS_CATS_JS_axrf000.cats]
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+//                                                                    //.
+//                         Applied Type System                        //.
+//                                                                    //.
+////////////////////////////////////////////////////////////////////////.
+
+/*
+** ATS/Xanadu - Unleashing the Potential of Types!
+** Copyright (C) 2024 Hongwei Xi, ATS Trustful Software, Inc.
+** All rights reserved
+**
+** ATS is free software;  you can  redistribute it and/or modify it under
+** the terms of  the GNU GENERAL PUBLIC LICENSE (GPL) as published by the
+** Free Software Foundation; either version 3, or (at  your  option)  any
+** later version.
+** 
+** ATS is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without  even  the  implied  warranty  of MERCHANTABILITY or
+** FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License
+** for more details.
+** 
+** You  should  have  received  a  copy of the GNU General Public License
+** along  with  ATS;  see the  file COPYING.  If not, please write to the
+** Free Software Foundation,  51 Franklin Street, Fifth Floor, Boston, MA
+** 02110-1301, USA.
+*/
+
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+/*
+Author: Hongwei Xi
+Thu 15 Aug 2024 01:42:20 PM EDT
+Authoremail: gmhwxiATgmailDOTcom
+*/
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+//
+function
+XATS2JS_a1sz_length
+  (A, i)
+{
+  return A.length // HX: length field!
+}
+//
+////////////////////////////////////////////////////////////////////////.
+//
+function
+XATS2JS_a1sz_lget$at
+  (A, i)
+{
+  return A[i]
+}
+function
+XATS2JS_a1sz_lset$at
+  (A, i, x)
+{
+  A[i] = x; return // HX: void is returned!
+}
+//
+////////////////////////////////////////////////////////////////////////.
+//
+/*
+HX-2024-08-15:
+Thu 15 Aug 2024 01:50:45 PM EDT
+*/
+//
+function
+XATS2JS_a1sz_make_ncpy
+  (n, x)
+{
+  var i = 0
+  var A = new Array(n);
+  while (i < n) {
+    A[i] = (x); i += 1
+  }
+  return A // HX: A = [x, x, ..., x]
+}
+//
+function
+XATS2JS_a1sz_make_nfun
+  (n, f)
+{
+  var i = 0
+  var A = new Array(n);
+  while (i < n) {
+    A[i] = f(i); i += 1
+  }
+  return A // HX: A = [f(0),f(1),...,f(n)]
+}
+//
+////////////////////////////////////////////////////////////////////////.
+//
+function
+XATS2JS_a1sz_fmake_fwork
+  (fwork)
+{
+  var A0 = []
+  fwork((x0) => {A0.push(x0);return}); return A0
+}
+//
+////////////////////////////////////////////////////////////////////////.
+////////////////////////////////////////////////////////////////////////.
+// end of [ATS3/XANADU_prelude_DATS_CATS_JS_axsz000.cats]
 ////////////////////////////////////////////////////////////////////////.
 ////////////////////////////////////////////////////////////////////////.
