d14262f99d7808117efb85c5c138789270ff05b8
Hongwei <gmhwxi@gmail.com>
2026-06-25 15:16:27 -0400

    Updating: very very minorly

---
+++ srcgen2/boot1_xjsemit.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+##################################################################
+MAKE="make"
+NODE="node"
+##################################################################
+prog=`basename $0`
+##################################################################
+#
+if [ ! "$XATSHOME" ] ; then
+  echo "XATSHOME should have been defined!"; exit 1
+fi
+#
+##################################################################
+#
+if [ ! -d "$XATSHOME" ] ; then
+  echo "XATSHOME should have been available at '${XATSHOME}'";
+  exit 1
+fi
+#
+##################################################################
+#
+case $prog in
+  boot1_xjsemit.sh)
+    exec ${MAKE} -f Makefile_xjsemit lib2xatsopt
+    exec ${MAKE} -C xats2js/srcgen1 -f Makefile_xjsemit lib2xatsopt
+    exec ${MAKE} -C xats2js/srcgen1/UTIL -f Makefile_xjsemit xats2js_jsemit00
+    ;;
+  *)
+    echo "prog: " $prog
+    echo "This is a script for build_xjsemit."
+    echo "The script should be symbolically named as [xats2js]."
+    exit 1
+    ;;
+esac
+#
+##################################################################
+#
+# end of [$(XATSHOME)/xassets/bin/xats2js_node.sh]
+#
+##################################################################
