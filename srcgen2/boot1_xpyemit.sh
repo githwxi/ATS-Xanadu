e381a64f45c7442e7ed4765d01d42ac474c46cf0
Hongwei <gmhwxi@gmail.com>
2026-06-25 18:25:15 -0400

    Updating: very very minorly

--- srcgen2/boot1_xpyemit.sh
+++ srcgen2/boot1_xpyemit.sh
@@ -19,23 +19,23 @@ fi
 ##################################################################
 #
 case $prog in
-  boot1_xjsemit.sh)
-    ${MAKE} -f Makefile_xjsemit lib2xatsopt
-    ${MAKE} -C xats2js/srcgen1 -f Makefile_xjsemit lib2xats2js
-    ${MAKE} -C xats2js/srcgen1/UTIL -f Makefile_xjsemit xats2js_jsemit00
+  boot1_xpyemit.sh)
+    ${MAKE} -f Makefile_xpyemit lib2xatsopt
+    ${MAKE} -C xats2js/srcgen1 -f Makefile_xpyemit lib2xats2py
+    ${MAKE} -C xats2js/srcgen1/UTIL -f Makefile_xpyemit xats2py_pyemit00
     echo "Please find: (./xats2js/srcgen1/UTIL/xats2js_jsemit00_ats3.js)!"
     exit 0
     ;;
   *)
     echo "prog: " $prog
     echo "This is a script for build_xjsemit."
-    echo "The script should be symbolically named as [boot1_xjsemit.sh]."
+    echo "The script should be symbolically named as [boot1_xpyemit.sh]."
     exit 1
     ;;
 esac
 #
 ##################################################################
 #
-# end of [ATS-Xanadu/srcgen2/boot1_xjsemit.sh]
+# end of [ATS-Xanadu/srcgen2/boot1_xpyemit.sh]
 #
 ##################################################################
